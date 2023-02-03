87082e28e72a03f261cd6190a2bb8d61d45fe77b
geiselrocha <geiselrocha@hotmail.com>
2023-02-02 22:28:37 -0300

    feat: generate the complete version of the report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
-class CompleteReport:
-    pass
+from typing import List, Dict
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data: List[Dict]):
+        result = SimpleReport.generate(data)
+
+        companies = []
+
+        for item in data:
+            companies.append(item["nome_da_empresa"])
+        companies_list = list(Counter(companies).items())
+        result = f"""{result}Produtos estocados por empresa:"""
+
+        for item in companies_list:
+            result = f"""{result}- {item[0]}: {str(item[1])}"""
+        return result
