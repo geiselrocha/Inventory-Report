384ab2f7cf82f494750d3ed0c8eacc9b69f4b8d4
geiselrocha <geiselrocha@hotmail.com>
2023-02-02 22:41:01 -0300

    fix: format on complete_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -13,8 +13,11 @@ class CompleteReport(SimpleReport):
         for item in data:
             companies.append(item["nome_da_empresa"])
         companies_list = list(Counter(companies).items())
-        result = f"""{result}Produtos estocados por empresa:"""
+        result = f"""{result}
+Produtos estocados por empresa:
+"""
 
         for item in companies_list:
-            result = f"""{result}- {item[0]}: {str(item[1])}"""
+            result = f"""{result}- {item[0]}: {str(item[1])}
+"""
         return result
