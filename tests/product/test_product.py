0f5bb9ed3fd9338833d1bd852c53ed606b0eaa29
geiselrocha <geiselrocha@hotmail.com>
2023-02-02 21:06:42 -0300

    test: function test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Panetone',
+        'Errepi Foods',
+        '01-01-2023',
+        '01-10-2023',
+        '64433725',
+        'armazenar em local arejado'
+        )
+    assert product.id == 1
+    assert product.nome_do_produto == 'Panetone'
+    assert product.nome_da_empresa == 'Errepi Foods'
+    assert product.data_de_fabricacao == '01-01-2023'
+    assert product.data_de_validade == '01-10-2023'
+    assert product.numero_de_serie == '64433725'
+    assert product.instrucoes_de_armazenamento == 'armazenar em local arejado'
