ee2fa2517f11ca10c3d6192a4dd686923af7b271
geiselrocha <geiselrocha@hotmail.com>
2023-02-02 21:45:09 -0300

    feat: generate the simplified version of the report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from typing import List, Dict
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data: List[Dict]):
+        manufacturing_date = min(
+            data, key=lambda item: item.get('data_de_fabricacao'))
+        expiration_date = min(
+            data, key=lambda item: item.get('data_de_validade'))
+
+        companies = []
+
+        for item in data:
+            companies.append(item["nome_da_empresa"])
+        companies_list = Counter(companies).most_common()[0][0]
+
+        print(companies_list)
+
+        return (
+            f"""Data de fabricação mais antiga: {manufacturing_date
+            ['data_de_fabricacao']}
+            Data de validade mais próxima: {expiration_date
+            ['data_de_validade']}
+            Empresa com mais produtos: {companies_list}""")
