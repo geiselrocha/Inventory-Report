03e4a043d4e9cffd130c484c382c0038088fc508
geiselrocha <geiselrocha@hotmail.com>
2023-02-05 00:08:05 -0300

    test: function for test the __repr__ implementation on Product class

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -6,15 +6,15 @@ def test_cria_produto():
         1,
         'Panetone',
         'Errepi Foods',
-        '01-01-2023',
-        '01-10-2023',
+        '2023-01-01',
+        '2023-11-11',
         '64433725',
-        'armazenar em local arejado'
+        'em local arejado'
         )
     assert product.id == 1
     assert product.nome_do_produto == 'Panetone'
     assert product.nome_da_empresa == 'Errepi Foods'
-    assert product.data_de_fabricacao == '01-01-2023'
-    assert product.data_de_validade == '01-10-2023'
+    assert product.data_de_fabricacao == '2023-01-01'
+    assert product.data_de_validade == '2023-11-11'
     assert product.numero_de_serie == '64433725'
-    assert product.instrucoes_de_armazenamento == 'armazenar em local arejado'
+    assert product.instrucoes_de_armazenamento == 'em local arejado'
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        2,
+        'Chocolate',
+        'Errepi Foods',
+        '2023-01-01',
+        '2023-12-12',
+        '19101909',
+        'em local arejado',
+    )
+
+    assert product.__repr__() == (
+        'O produto Chocolate'
+        ' fabricado em 2023-01-01'
+        ' por Errepi Foods com validade'
+        ' até 2023-12-12'
+        ' precisa ser armazenado em local arejado.'
+    )
